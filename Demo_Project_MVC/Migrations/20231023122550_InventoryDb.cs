9b802587b673c84944b4289af6b5dd044da757b6
Prey36 <preydalsaniya@gmail.com>
2023-10-24 16:49:59 +0530

    first commit

---
+++ Demo_Project_MVC/Migrations/20231023122550_InventoryDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo_Project_MVC.Migrations
+{
+    public partial class InventoryDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
